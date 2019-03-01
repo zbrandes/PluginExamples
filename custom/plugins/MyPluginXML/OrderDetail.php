7e068081c2c215cbd59fe701f28d5dde484c9436
zbrandes <48127090+zbrandes@users.noreply.github.com>
2019-03-01 23:02:14 +0100

    Update OrderDetail.php

--- custom/plugins/MyPluginXML/OrderDetail.php
+++ custom/plugins/MyPluginXML/OrderDetail.php
@@ -8,13 +8,9 @@ $orderdata = $xml->addChild('Oderdata');
 $orderdata->addChild('OrderNumber', '20176');
 $orderdata->addChild('OrderID', '3');
 
-
-
 $basketdata = $xml->addChild('Basketdata');
 $basketdata->addChild('Articlenumber', 'BS_01');
 $basketdata->addChild('Articlename', 'BOOTCUT JEANS FÜR DAMEN inch31');
-
-
 
 $customerdata = $xml->addChild('Customerdata');
 $customerdata->addChild('Firstname', 'Evgenij');
@@ -23,8 +19,6 @@ $customerdata->addChild('Street', 'Lembergweg');
 $customerdata->addChild('Zipcode', '71067');
 $customerdata->addChild('City', 'Sindelfingen');
 
-
-
 $shippingdata = $xml->addChild('Shippingdata');
 $shippingdata->addChild('Firstname', 'Evgenij');
 $shippingdata->addChild('Lastname', 'Brandes');
@@ -32,13 +26,9 @@ $shippingdata->addChild('Street', 'Lembergweg');
 $shippingdata->addChild('Zipcode', '71067');
 $shippingdata->addChild('City', 'Sindelfingen');
 
-
-
 $paymentdata = $xml->addChild('Paymentdata');
 $paymentdata->addChild('Name', 'DebitPayment');
 $paymentdata->addChild('Description', 'Maestro');
-
-
 
 $costdata = $xml->addChild('Costdata');
 $costdata->addChild('ShiipingCost', '3.00');
@@ -46,8 +36,6 @@ $costdata->addChild('Price', '15.00');
 $costdata->addChild('NetPrice', '13.79');
 $costdata->addChild('Quantity', '1');
 $costdata->addChild('Tax', '19');
-
-
 
 $xmlDOM = dom_import_simplexml($xml)->ownerDocument;
 $xmlDOM->formatOutput = true;
